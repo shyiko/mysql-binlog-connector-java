57e7dad5cc33e8e0b32668cbe216edff39853659
Stanley Shyiko <stanley.shyiko@gmail.com>
2013-06-24 14:53:45 +0300

    Added BinaryLogClient.LifecycleListener.onCommunicationFailure

--- src/main/java/com/github/shyiko/mysql/binlog/BinaryLogClient.java
+++ src/main/java/com/github/shyiko/mysql/binlog/BinaryLogClient.java
@@ -174,6 +174,12 @@ public class BinaryLogClient {
                 }
                 notifyEventListeners(event);
             }
+        } catch (Exception e) {
+            synchronized (lifecycleListeners) {
+                for (LifecycleListener lifecycleListener : lifecycleListeners) {
+                    lifecycleListener.onCommunicationFailure(this, e);
+                }
+            }
         } finally {
             if (connected) {
                 disconnect();
@@ -264,8 +270,26 @@ public class BinaryLogClient {
      */
     public interface LifecycleListener {
 
+        /**
+         * Called once client has successfully logged in but before started to receive binlog events.
+         */
         void onConnect(BinaryLogClient client);
+
+        /**
+         * It's guarantied to be called before {@link #onDisconnect(BinaryLogClient)}) in case of
+         * communication failure.
+         */
+        void onCommunicationFailure(BinaryLogClient client, Exception ex);
+
+        /**
+         * Called in case of failed event deserialization. Note this type of error does NOT cause client to
+         * disconnect. If you wish to stop receiving events you'll need to fire client.disconnect() manually.
+         */
         void onEventDeserializationFailure(BinaryLogClient client, Exception ex);
+
+        /**
+         * Called upon disconnect (regardless of the reason).
+         */
         void onDisconnect(BinaryLogClient client);
     }
 
