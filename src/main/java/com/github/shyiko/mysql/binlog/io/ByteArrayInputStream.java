f57fd59233d5821e59987195514117f6b00640fa
Stanley Shyiko <stanley.shyiko@gmail.com>
2015-03-30 20:45:32 +0300

    Fixed #34 - A bug in ByteArrayInputStream (reverse)

--- src/main/java/com/github/shyiko/mysql/binlog/io/ByteArrayInputStream.java
+++ src/main/java/com/github/shyiko/mysql/binlog/io/ByteArrayInputStream.java
@@ -107,7 +107,7 @@ public class ByteArrayInputStream extends InputStream {
     }
 
     private byte[] reverse(byte[] bytes) {
-        for (int i = 0, length = bytes.length >> 2; i <= length; i++) {
+        for (int i = 0, length = bytes.length >> 1; i < length; i++) {
             int j = bytes.length - 1 - i;
             byte t = bytes[i];
             bytes[i] = bytes[j];
