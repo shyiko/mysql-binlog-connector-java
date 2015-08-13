1a7b09d258a829572c0276c53c18c47dec286c9b
Stanley Shyiko <stanley.shyiko@gmail.com>
2015-08-13 14:10:28 -0700

    Corrected MissingTableMapEventException error message

--- src/main/java/com/github/shyiko/mysql/binlog/event/deserialization/AbstractRowsEventDataDeserializer.java
+++ src/main/java/com/github/shyiko/mysql/binlog/event/deserialization/AbstractRowsEventDataDeserializer.java
@@ -78,7 +78,7 @@ public abstract class AbstractRowsEventDataDeserializer<T extends EventData> imp
         TableMapEventData tableMapEvent = tableMapEventByTableId.get(tableId);
         if (tableMapEvent == null) {
             throw new MissingTableMapEventException("No TableMapEventData has been found for table id:" + tableId +
-                ". Usually that means that you have started reading binary log 'within the logic group'" +
+                ". Usually that means that you have started reading binary log 'within the logical event group'" +
                 " (e.g. from WRITE_ROWS and not proceeding TABLE_MAP");
         }
         byte[] types = tableMapEvent.getColumnTypes();
