0e6500389aa725eefa4ea8fbca1ceee7772773c7
Stanley Shyiko <stanley.shyiko@gmail.com>
2016-01-15 13:27:36 -0800

    Fixed #84 - keep alive thread interfering with non-blocking mode

--- src/main/java/com/github/shyiko/mysql/binlog/BinaryLogClient.java
+++ src/main/java/com/github/shyiko/mysql/binlog/BinaryLogClient.java
@@ -610,6 +610,7 @@ public class BinaryLogClient implements BinaryLogClientMXBean {
 
     private void listenForEventPackets() throws IOException {
         ByteArrayInputStream inputStream = channel.getInputStream();
+        boolean completeShutdown = false;
         try {
             while (inputStream.peek() != -1) {
                 int packetLength = inputStream.readInteger(3);
@@ -621,6 +622,7 @@ public class BinaryLogClient implements BinaryLogClientMXBean {
                         errorPacket.getSqlState());
                 }
                 if (marker == (byte) 0xFE && !blocking) {
+                    completeShutdown = true;
                     break;
                 }
                 Event event;
@@ -658,7 +660,11 @@ public class BinaryLogClient implements BinaryLogClientMXBean {
             }
         } finally {
             if (isConnected()) {
-                disconnectChannel();
+                if (completeShutdown) {
+                    disconnect(); // initiate complete shutdown sequence (which includes keep alive thread)
+                } else {
+                    disconnectChannel();
+                }
             }
         }
     }
