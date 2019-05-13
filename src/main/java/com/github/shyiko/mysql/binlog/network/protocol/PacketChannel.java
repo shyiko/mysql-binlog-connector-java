3898aca8d675d5fd7e0a09a4515ff28a7ab9375d
glarwood <gabriel@fivetran.com>
2019-05-13 16:49:48 -0700

    fix(ByteArrayOutputStream): use composed OutputStream#write

--- src/main/java/com/github/shyiko/mysql/binlog/network/protocol/PacketChannel.java
+++ src/main/java/com/github/shyiko/mysql/binlog/network/protocol/PacketChannel.java
@@ -67,6 +67,7 @@ public class PacketChannel implements Channel {
         buffer.writeInteger(body.length, 3); // packet length
         buffer.writeInteger(packetNumber, 1);
         buffer.write(body, 0, body.length);
+        buffer.flush();
         outputStream.write(buffer.toByteArray());
         // though it has no effect in case of default (underlying) output stream (SocketOutputStream),
         // it may be necessary in case of non-default one
